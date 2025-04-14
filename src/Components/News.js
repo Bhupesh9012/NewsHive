7ec462a6bd0f7699a1a618dc23c766098ce89543
Bhupeshjoshi07 <153195802+Bhupesh9012@users.noreply.github.com>
2025-04-14 11:03:47 +0530

    aster

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,17 +7,49 @@ export class News extends Component {
     super();
     this.state ={
       articles:[],
-      loading:false
+      loading:false,
+      page:1
     }
   }
   async componentDidMount(){
     
-    let url="https://newsapi.org/v2/everything?q=india&apiKey=2a0547b144c14f7cbb742448fcf7bfb9";
+    let url="https://newsapi.org/v2/everything?q=india&apiKey=2a0547b144c14f7cbb742448fcf7bfb9&pageSize=20";
     let data = await fetch(url);
     let parsedData =await data.json()
     console.log(parsedData);
-    this.setState({articles:parsedData.articles})
+    this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
   }
+  handlePreviousClick= async()=>{
+    console.log("Previous");
+    let url=`https://newsapi.org/v2/everything?q=india&apiKey=2a0547b144c14f7cbb742448fcf7bfb9&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData =await data.json()
+    console.log(parsedData);
+    this.setState({
+      page:this.state.page - 1,
+      articles:parsedData.articles
+    })
+    
+
+  }
+ handleNextClick=async()=>{
+    console.log("Next");
+    if(this.state.page + 1>Math.ceil(this.state.totalResultstotalResults/20)){
+
+    }
+    else{
+    let url=`https://newsapi.org/v2/everything?q=india&apiKey=2a0547b144c14f7cbb742448fcf7bfb9&page=${this.state.page + 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData =await data.json()
+    console.log(parsedData);
+    this.setState({
+      page:this.state.page + 1,
+      articles:parsedData.articles
+    })
+  }
+
+  }
+  
   render() {
     return (
           <div className="container my-3">
@@ -28,7 +60,11 @@ export class News extends Component {
             <Newsitem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
             })}        
-        </div>    
+        </div>  
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePreviousClick}>&#8592; Previous</button>
+        <button disabled={this.state.page<=""}type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &#8594;</button>
+        </div>  
       </div>
     )
   }
