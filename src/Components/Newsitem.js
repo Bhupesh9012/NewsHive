9dda3d6e02dd6cdefb9679ead9871707e1579559
Bhupeshjoshi07 <153195802+Bhupesh9012@users.noreply.github.com>
2025-04-25 08:13:45 +0530

    aster

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -6,9 +6,10 @@ export class Newsitem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-        <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
-                {source}
+          <div style={{display:'flex',justifyContent:'flex-end',position:'absolute',right:'0'}}>
+        <span className=" badge rounded-pill bg-danger">{source}
               </span>
+            </div>
           <img
             src={
               !imageUrl
